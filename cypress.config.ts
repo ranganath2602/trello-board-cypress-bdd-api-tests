d87dbcf5a3ad0e6d3cdaa3a8764f1703bd6b8ba0
Ranganath L <ranganath.l.2001@gmail.com>
2025-12-22 16:06:49 +0530

    correct ci file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'cypress';
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
-import webpackPreprocessor from '@cypress/webpack-preprocessor';
 import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
 import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
 
@@ -9,20 +8,29 @@ export default defineConfig({
     baseUrl: 'https://api.trello.com',
     specPattern: 'cypress/e2e/**/*.feature',
     supportFile: 'cypress/support/index.ts',
+
     env: {
-      TRELLO_KEY: process.env.TRELLO_KEY || 'cd8ce8f5f3ef878d11927d43cc325cae',
-      TRELLO_TOKEN: process.env.TRELLO_TOKEN || 'ATTA3714dedd8931ba02ec988baf032354b7c6fe209c69c5c66e1ec96df9576039dc05C9E21D',
+      TRELLO_KEY: process.env.TRELLO_KEY,
+      TRELLO_TOKEN: process.env.TRELLO_TOKEN,
     },
+
     async setupNodeEvents(on, config) {
-      // required for @badeball preprocessor
+      // Required for Cucumber
       await addCucumberPreprocessorPlugin(on, config);
 
-      // Use esbuild bundler with the cucumber preprocessor plugin
-      const bundler = createBundler({
-        plugins: [createEsbuildPlugin(config)],
-      });
-
-      on('file:preprocessor', bundler);
+      // Esbuild bundler with Node crypto fix
+      on(
+        'file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+          platform: 'node',
+          target: 'node18',
+          define: {
+            crypto: 'require("crypto")',
+          },
+          external: ['esbuild'],
+        })
+      );
 
       return config;
     },
